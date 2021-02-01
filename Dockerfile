e2c9cf088304ef5c62f1683b0fc25a57af96c0e3
Pablo Fuset Sanz <pablofuset@gmail.com>
2021-02-01 23:18:22 +0100

    Works local

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,13 @@
 FROM node:latest
 
 RUN apt-get update && apt-get install -y zip
+RUN apt-get install git -y
 
 COPY ./git_diff_script.sh /
 
+RUN git init
+RUN git remote add origin https://github.com/pfusetsa/git-diff-repo.git
+RUN git fetch
 RUN chmod +x /git_diff_script.sh
 
-ENTRYPOINT ["/git_diff_script.sh"]
+ENTRYPOINT ["sh", "/git_diff_script.sh"]
--- git_diff_script.sh
+++ git_diff_script.sh
@@ -1,30 +1,43 @@
-#!/bin/bash
+#!/bin/sh
 
+#Auxiliary function
+copy_file() {
+    echo "Función alcanzada"
+    file_name=$1
+    commit_id=$2
 
-function copy_file {
-    local file_name = $1
-    local commit_id = $2
+    echo $file_name
 
-    git show "$commit_id" "$file_name" > "$file_name"
+    if [ ! -d "Git_Diff" ]
+    then
+        mkdir "Git_Diff"
+    fi
+
+    git show $commit_id $file_name > Git_Diff/$file_name
+
 }
 
+#Get input variables
+originalCommit=314930d7d2d1435831eabc921897fcbefffeb3d9
+newCommit=53c75a35cd46dce05d046094e81f3736dded8d34
 
-originalCommit = $1
-newCommit = $2
+#Create differences file (list of changed files)
+git diff $originalCommit $newCommit --name-only > Git_Diff_Changes.txt
 
-if [ ! -d "Git_Diff" ]
-then
-mkdir "Git_Diff"
-fi
-
-cd Git_Diff
-
-git diff "$originalCommit" "$newCommit" --name-only > Git_Diff_Changes.txt
-
-files = $(git diff "$originalCommit" "$newCommit" --name-only)
-
-echo "$files" | while read line; do
-    copy_file "$newCommit" "${line}"
+#Copy files into new directory
+cat Git_Diff_Changes.txt | while read line; do
+    echo "Instancia bucle"
+    copy_file ${line} $newCommit 
 done
+
+#Write full git diff data into Git_Diff_Changes.txt
+git diff $originalCommit $newCommit > Git_Diff_Changes.txt
+mv Git_Diff_Changes.txt Git_Diff
+
+#Zip files
+zip -r git_diff.zip Git_Diff
+
+#Delete original folder
+#rm -r Git_Diff
 
 exit
