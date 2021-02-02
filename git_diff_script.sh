b89f14c0883bb28d9790953f7803099a258ddee5
Pablo Fuset Sanz <pfusetsa@everis.com>
2021-02-02 11:01:55 +0100

    SH modif

--- git_diff_script.sh
+++ git_diff_script.sh
@@ -4,7 +4,6 @@
 copy_file() {
     echo "Función alcanzada"
     file_name=$1
-    commit_id=$2
 
     echo $file_name
 
@@ -13,29 +12,25 @@ copy_file() {
         mkdir "Git_Diff"
     fi
 
-    git show $commit_id $file_name > Git_Diff/$file_name
+    git show $file_name > Git_Diff/$file_name
 
 }
 
-#Get input variables
-originalCommit=314930d7d2d1435831eabc921897fcbefffeb3d9
-newCommit=53c75a35cd46dce05d046094e81f3736dded8d34
-
 #Create differences file (list of changed files)
-git diff $originalCommit $newCommit --name-only > Git_Diff_Changes.txt
+git diff --name-only > Git_Diff_Changes.txt
 
 #Copy files into new directory
 cat Git_Diff_Changes.txt | while read line; do
     echo "Instancia bucle"
-    copy_file ${line} $newCommit 
+    copy_file ${line} 
 done
 
 #Write full git diff data into Git_Diff_Changes.txt
-git diff $originalCommit $newCommit > Git_Diff_Changes.txt
+git diff > Git_Diff_Changes.txt
 mv Git_Diff_Changes.txt Git_Diff
 
 #Zip files
-zip -r git_diff.zip Git_Diff
+#zip -r git_diff.zip Git_Diff
 
 #Delete original folder
 #rm -r Git_Diff
